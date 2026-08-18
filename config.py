e1bf9f1002b71bfe06c55ec510fba7d87efda42e
Neha Jain <njnehajain330@gmail.com>
2026-08-18 22:54:00 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# App configuration
+DATABASE_URL = "sqlite:///taskflow.db"
+
+# TODO: move to env vars later
+API_KEY = "sk_live_9f8b7c6d5e4f3a2b1c0d9e8f7a6b5c4d"
+DB_PASSWORD = "SuperSecret123!"
+
+PAGE_SIZE = 20
